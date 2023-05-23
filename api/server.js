330cea575dfb534de2cd47415567bb99ca9f1310
Sultani <sultani@gamil.com>
2023-05-23 16:12:11 +0430

    installing nodemon package

--- api/server.js
+++ api/server.js
@@ -7,6 +7,12 @@ let app=express();
 app.use(express.json);
 app.use(cors());
 
+mongoose.connect("mongodb://127.0.0.1:27017/todolist",{
+    useNewUrlParser:true
+}).then(()=>{
+    console.log("db is connected")
+})
+
 
 app.listen('8000',()=>{
     console.log("server is running")
